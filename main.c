cdff3b12973d461167a549c20334ec500345c977
Bob-Z <carabobz@gmail.com>
2015-10-31 10:18:58 +0100

    Update black soft list with x07_cass

--- main.c
+++ main.c
@@ -73,7 +73,7 @@ char * chd_black_list[] = { "pcecd", "cdi", "pippin", NULL} ;
 /* Names of drivers to skip when running in auto mode */
 char * auto_black_list[] = { "cdimono1", "cpc464", "cpc664", "cpc6128", "al520ex", "kccomp", "cpc6128f", "cpc6128s", "bbcb_de", NULL } ;
 /* Names of softlist to skip when running in auto mode */
-char * auto_black_softlist[] = { "tvc_flop", "ti99_cart", "bbca_cass", "bbcb_cass", "msx1_cass", "lviv", "kc_cass", "spc1000_cass", "sol20_cass", "mtx_cass", "dai_cass", "ep64_flop", "pet_cass", "orion_cass", "mz700_cass", "special_cass", "cbm2_flop", "cgenie_cass", "jupace_cass", "bbcm_flop", "fm7_cass", "mc10", NULL } ;
+char * auto_black_softlist[] = { "tvc_flop", "ti99_cart", "bbca_cass", "bbcb_cass", "msx1_cass", "lviv", "kc_cass", "spc1000_cass", "sol20_cass", "mtx_cass", "dai_cass", "ep64_flop", "pet_cass", "orion_cass", "mz700_cass", "special_cass", "cbm2_flop", "cgenie_cass", "jupace_cass", "bbcm_flop", "fm7_cass", "mc10", "x07_cass", NULL } ;
 /* Names of drivers to skip when selecting a driver */
 char * driver_black_list[] = { "kccomp", "al520ex", "cpc6128s", "cpc6128f", NULL };
 /* Description string to black list (namely for fruit machine) */
